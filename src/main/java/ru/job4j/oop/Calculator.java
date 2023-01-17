09221a54eb4a596076e6ff68c843adef32d3f6b9
devseag <ageevvlsergey@gmail.com>
2023-01-16 20:34:47 -0400

    Repository for Tracker project

---
+++ src/main/java/ru/job4j/oop/Calculator.java
@@ -0,0 +1,37 @@
+package ru.job4j.oop;
+
+public class Calculator {
+
+    private static int x = 5;
+
+    public static int sum(int y) {
+        return x + y;
+    }
+
+    public static int minus(int y) {
+        return y - x;
+    }
+
+    public int divide(int y) {
+        return y / x;
+    }
+
+    public int sumAllOperation(int y) {
+        return sum( sum(y)  + minus(y) + divide(y) + multiply(y));
+    }
+
+    public int multiply(int a) {
+        return x * a;
+    }
+
+    public static void main(String[] args) {
+        int result = Calculator.sum(10);
+        System.out.println(result);
+        Calculator calculator = new Calculator();
+        int rsl = calculator.multiply(5);
+        System.out.println(rsl);
+        System.out.println(minus(15));
+        System.out.println(calculator.divide(20));
+        System.out.println(calculator.sumAllOperation(30));
+    }
+}
