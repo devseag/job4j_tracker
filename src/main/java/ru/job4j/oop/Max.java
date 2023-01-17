d21d8c8563bb4a6afc5d5f6244e4fc12c7ff40cf
devseag <ageevvlsergey@gmail.com>
2023-01-16 22:19:14 -0400

    Repository for Tracker project

---
+++ src/main/java/ru/job4j/oop/Max.java
@@ -0,0 +1,23 @@
+package ru.job4j.oop;
+
+public class Max {
+    public static int max(int left, int right) {
+        int result = left > right ? left : right;
+        return result;
+    }
+
+    public static int max(int left, int right, int third) {
+        //int result = third > max(left, right) ? third : max(left, right);
+        return max(max(left,right), third);
+    }
+
+    public static int max(int left, int right, int third, int fourth) {
+        //int result = fourth > max(left, right, third) ? fourth : max(left, right, third);
+        return max(max(left, right, third), fourth);
+    }
+
+    public static int summation(int first, int second) {
+        int result = first + second;
+        return result;
+    }
+}
