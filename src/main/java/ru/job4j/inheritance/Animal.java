f2378c1c8f002753ad35405da374b03b17e89893
devseag <ageevvlsergey@gmail.com>
2023-01-26 13:25:49 -0400

    a few changes for project

---
+++ src/main/java/ru/job4j/inheritance/Animal.java
@@ -0,0 +1,11 @@
+package ru.job4j.inheritance;
+
+public class Animal {
+    public void instanceInvoke() {
+        System.out.println("Run method Animal instance");
+    }
+
+    public static void staticInvoke() {
+        System.out.println("Run static method Animal");
+    }
+}
--- src/main/java/ru/job4j/inheritance/Cat.java
+++ src/main/java/ru/job4j/inheritance/Cat.java
@@ -1,8 +1,16 @@
 package ru.job4j.inheritance;
 
-public class Cat {
+public class Cat extends Animal {
+    @Override
+    public void instanceInvoke() {
+        System.out.println("Run method Cat instance");
+    }
+
+    public static void staticInvoke() {
+        System.out.println("Run static method Cat");
+    }
+
     public boolean canPurr() {
         return true;
     }
 }
-
