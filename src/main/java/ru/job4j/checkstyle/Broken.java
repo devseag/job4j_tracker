137f5d92ff35e2147db22cbc90954eb0695d6021
devseag <ageevvlsergey@gmail.com>
2023-01-26 15:27:40 -0400

    a few changes for project

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,24 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+
+    private int sizeOfEmpty = 10;
+    public String surname;
+    String name;
+    public static final String NEW_VALUE = "";
+
+    public Broken() { }
+
+    public void echo() { }
+
+    public void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
+
+    }
+
+}
