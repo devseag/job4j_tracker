439479cfc30c50c2df1a26a7d3f61637c4c27643
devseag <ageevvlsergey@gmail.com>
2023-01-28 19:46:53 -0400

    a few changes for project

--- src/main/java/ru/job4j/varargs/VarArgs.java
+++ src/main/java/ru/job4j/varargs/VarArgs.java
@@ -10,11 +10,25 @@ public class VarArgs {
         return builder + " Number of joined elements: " + strings.length;
     }
 
+    public static String text(int x, String... strings) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(x);
+        for (String s : strings) {
+            builder.append(s);
+        }
+        return builder.toString();
+    }
+
+    public static String text(String str) {
+        return str;
+    }
+
     public static void main(String[] args) {
         System.out.println(text());
         System.out.println(text("aaa"));
         System.out.println(text("aaa", "bbb", "ccc"));
         System.out.println(text("aaa", "bbb", "ccc", "ddd", "eee"));
+        System.out.println(text(30, "aaa", "bbb"));
     }
 
 }
