47192e5af0f482fd5608703a8ef94b2bbf7f055b
devseag <ageevvlsergey@gmail.com>
2023-02-03 16:18:08 -0400

    a few changes for project

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,26 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (key.equals(value[i])) {
+                rsl = i;
+                break;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("key is not found");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+            System.out.println(indexOf(new String[]{"test1", "test2", "test3"}, "test2"));
+            System.out.println(indexOf(new String[]{"test1", "test2", "test3"}, "test5"));
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
