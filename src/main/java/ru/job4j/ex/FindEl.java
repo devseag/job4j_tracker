1a71274f42310f0958957514b3a1ebffc05c5d71
devseag <ageevvlsergey@gmail.com>
2023-02-03 20:20:34 -0400

    a few changes for project

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -1,26 +1,30 @@
 package ru.job4j.ex;
 
 public class FindEl {
-    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+    public static int indexOf(String[] values, String key) throws ElementNotFoundException {
         int rsl = -1;
-        for (int i = 0; i < value.length; i++) {
-            if (key.equals(value[i])) {
-                rsl = i;
-                break;
-            }
-        }
-        if (rsl == -1) {
-            throw new ElementNotFoundException("key is not found");
-        }
+        /* for-each */
         return rsl;
     }
 
-    public static void main(String[] args) {
+    public static boolean sent(String value, String[] abuses) throws ElementAbuseException {
+        /* if contains throw ElementAbuseException */
+        return true;
+    }
+
+    public static void process(String[] values, String key, String[] abuses) {
         try {
-            System.out.println(indexOf(new String[]{"test1", "test2", "test3"}, "test2"));
-            System.out.println(indexOf(new String[]{"test1", "test2", "test3"}, "test5"));
-        } catch (ElementNotFoundException e) {
+            if (indexOf(values, key) != -1) {
+                sent(key, abuses);
+            }
+        } catch (ElementAbuseException ea) {
+            ea.printStackTrace();
+        } catch (ElementNotFoundException en) {
+            en.printStackTrace();
+        } catch (Exception e) {
             e.printStackTrace();
+        } catch (Throwable th) {
+            th.printStackTrace();
         }
     }
 }
