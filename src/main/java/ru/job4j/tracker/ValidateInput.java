5c5c6ed4d3055ac76e81a62a657e9d2625f068b4
devseag <ageevvlsergey@gmail.com>
2023-02-03 22:32:00 -0400

    a few changes for project

--- src/main/java/ru/job4j/tracker/ValidateInput.java
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -1,6 +1,18 @@
 package ru.job4j.tracker;
 
-public class ValidateInput extends ConsoleInput {
+public class ValidateInput implements Input {
+    private final Output out;
+    private final Input in;
+
+    public ValidateInput(Output out, Input input) {
+        this.out = out;
+        this.in = input;
+    }
+
+    @Override
+    public String askStr(String question) {
+        return in.askStr(question);
+    }
 
     @Override
     public int askInt(String question) {
@@ -8,12 +20,39 @@ public class ValidateInput extends ConsoleInput {
         int value = -1;
         do {
             try {
-                value = super.askInt(question);
+                value = in.askInt(question);
                 invalid = false;
             } catch (NumberFormatException nfe) {
-                System.out.println("Please enter valid data");
+                out.println("Please enter valid data");
             }
         } while (invalid);
         return value;
     }
+//    @Override
+//    public int askInt(String question) {
+//        boolean invalid = true;
+//        int value = -1;
+//        do {
+//            String rsl = in.askStr(question);
+//            if (!isNumber(rsl)) {
+//                System.out.println("Please enter valid data");
+//                continue;
+//            }
+//            value = Integer.parseInt(rsl);
+//            invalid = false;
+//        } while (invalid);
+//        return value;
+//    }
+
+//    private boolean isNumber(String value) {
+//        boolean rsl = true;
+//        char[] check = value.toCharArray();
+//        for (char num : check) {
+//            if (num < 48 || num > 57) {
+//                rsl = false;
+//                break;
+//            }
+//        }
+//        return rsl;
+//    }
 }
