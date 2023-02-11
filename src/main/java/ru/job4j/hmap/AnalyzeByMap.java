bcc1a74254f9bc705051ef7ed984ca694fa6cf79
devseag <ageevvlsergey@gmail.com>
2023-02-10 21:07:58 -0400

    a few changes for project

--- src/main/java/ru/job4j/hmap/AnalyzeByMap.java
+++ src/main/java/ru/job4j/hmap/AnalyzeByMap.java
@@ -7,9 +7,9 @@ public class AnalyzeByMap {
         double cntElements = 0D;
         double sumSubjectScore = 0D;
         for (Pupil pupil : pupils) {
+            cntElements += pupil.subjects().size();
             for (Subject subject : pupil.subjects()) {
                 sumSubjectScore += subject.score();
-                cntElements++;
             }
         }
         return sumSubjectScore / cntElements;
@@ -18,39 +18,70 @@ public class AnalyzeByMap {
     public static List<Label> averageScoreByPupil(List<Pupil> pupils) {
         List<Label> labelList = new ArrayList<>();
         double sumSubjectScore = 0D;
-        double sumSubjectName = 0D;
         for (Pupil pupil : pupils) {
             for (Subject subject : pupil.subjects()) {
                 sumSubjectScore += subject.score();
-                sumSubjectName++;
             }
-            Label labels = new Label(pupil.name(), sumSubjectScore / sumSubjectName);
-            labelList.add(labels);
+            labelList.add(new Label(pupil.name(), sumSubjectScore / pupil.subjects().size()));
             sumSubjectScore = 0D;
-            sumSubjectName = 0D;
         }
-
         return labelList;
     }
 
     public static List<Label> averageScoreBySubject(List<Pupil> pupils) {
         Map<String, Integer> mapScoreBySubject = new LinkedHashMap<>();
-        double sumSubject = 0D;
+        List<Label> labelList = new ArrayList<>();
+        int sumSubjectScore = 0;
         for (Pupil pupil : pupils) {
             for (Subject subject : pupil.subjects()) {
-                sumSubject += subject.score();
-                mapScoreBySubject.put(subject.name(), (int) sumSubject);
+                if (mapScoreBySubject.containsKey(subject.name())) {
+                    sumSubjectScore += subject.score() + mapScoreBySubject.get(subject.name());
+                    mapScoreBySubject.put(subject.name(), sumSubjectScore);
+                    sumSubjectScore = 0;
+                } else {
+                    mapScoreBySubject.put(subject.name(), subject.score());
+                }
             }
-
         }
-        return List.of();
+        for (Map.Entry<String, Integer> entry : mapScoreBySubject.entrySet()) {
+            labelList.add(new Label(entry.getKey(), entry.getValue() / pupils.size()));
+        }
+        return labelList;
     }
 
     public static Label bestStudent(List<Pupil> pupils) {
-        return null;
+        List<Label> labelList = new ArrayList<>();
+        double sumSubjectScore = 0D;
+        for (Pupil pupil : pupils) {
+            for (Subject subject : pupil.subjects()) {
+                sumSubjectScore += subject.score();
+            }
+            labelList.add(new Label(pupil.name(), sumSubjectScore));
+            sumSubjectScore = 0D;
+        }
+        labelList.sort(Comparator.naturalOrder());
+        return labelList.get(labelList.size() - 1);
     }
 
     public static Label bestSubject(List<Pupil> pupils) {
-        return null;
+        Map<String, Integer> mapBestSubject = new LinkedHashMap<>();
+        List<Label> labelList = new ArrayList<>();
+        int sumSubjectScore = 0;
+        for (Pupil pupil : pupils) {
+            for (Subject subject : pupil.subjects()) {
+                if (mapBestSubject.containsKey(subject.name())) {
+                    sumSubjectScore += subject.score() + mapBestSubject.get(subject.name());
+                    mapBestSubject.put(subject.name(), sumSubjectScore);
+                    sumSubjectScore = 0;
+                } else {
+                    mapBestSubject.put(subject.name(), subject.score());
+                }
+            }
+        }
+        for (Map.Entry<String, Integer> entry : mapBestSubject.entrySet()) {
+            labelList.add(new Label(entry.getKey(), entry.getValue()));
+        }
+        labelList.sort(Comparator.naturalOrder());
+        return labelList.get(labelList.size() - 1);
     }
 }
