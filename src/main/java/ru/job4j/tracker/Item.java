3a4e51ba60708953dd16beb26274b5feddd8f57c
devseag <example@gmail.com>
2023-08-25 10:18:41 -0300

    add many to many

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,149 +1,59 @@
-//package ru.job4j.tracker;
-//
-//import ru.job4j.collection.Job;
-//
-//import java.time.LocalDateTime;
-//import java.time.format.DateTimeFormatter;
-//import java.util.Comparator;
-//import java.util.Objects;
-//
-//import lombok.Data;
-//
-//@Data
-//public class Item {
-//    private int id;
-//    private String name;
-//    private LocalDateTime created = LocalDateTime.now();
-//    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-//
-//    public Item() {
-//    }
-//
-//    public Item(String name) {
-//        this.name = name;
-//    }
-//
-//    public Item(int id, String name) {
-//        this.id = id;
-//        this.name = name;
-//    }
-//
-//    public Item(int id, String name, LocalDateTime created) {
-//        this.id = id;
-//        this.name = name;
-//        this.created = created;
-//    }
-//
-////    public int getId() {
-////        return id;
-////    }
-////
-////    public void setId(int id) {
-////        this.id = id;
-////    }
-////
-////    public String getName() {
-////        return name;
-////    }
-////
-////    public void setName(String name) {
-////        this.name = name;
-////    }
-////
-////    public LocalDateTime getCreated() {
-////        return created;
-////    }
-//
-//    @Override
-//    public String toString() {
-//        return "Item{"
-//                + "id=" + id
-//                + ", name='" + name + '\''
-//                + ", created=" + created.format(FORMATTER)
-//                + '}';
-////        return String.format("id: %s, name: %s, created: %s", id, name, FORMATTER.format(created));
-//    }
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) {
-//            return true;
-//        }
-//        if (o == null || getClass() != o.getClass()) {
-//            return false;
-//        }
-//        Item item = (Item) o;
-//        return Objects.equals(name, item.name);
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(name);
-//    }
-//}
-
-//package ru.job4j.tracker;
-//
-//import lombok.Data;
-//import javax.persistence.*;
-//
-//import java.time.LocalDateTime;
-//
-//@Entity
-//@Table(name = "items")
-//@Data
-//public class Item {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Integer id;
-//    private String name;
-//    private LocalDateTime created = LocalDateTime.now();
-//}
-
 package ru.job4j.tracker;
 
-import lombok.*;
-
-import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.*;
+import lombok.*;
+import ru.job4j.toone.User;
 
+@Data
+@NoArgsConstructor
 @Entity
 @Table(name = "items")
-@Getter
-@Setter
-@NoArgsConstructor(force = true)
-@AllArgsConstructor
-@RequiredArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@ToString(onlyExplicitlyIncluded = true)
 public class Item {
 
-    private static final DateTimeFormatter FORMATTER
-            = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+    private static final DateTimeFormatter FORMATTER =
+            DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
-    @ToString.Include
     private int id;
 
     @EqualsAndHashCode.Include
-    @ToString.Include
-    @NonNull
     private String name;
 
     private LocalDateTime created = LocalDateTime.now();
+
+    @ManyToMany
+    @JoinTable(
+            name = "participates",
+            joinColumns = { @JoinColumn(name = "item_id") },
+            inverseJoinColumns = { @JoinColumn(name = "user_id") }
+    )
+    private List<User> participates = new ArrayList<>();
+
+    public Item(String name) {
+        this.name = name;
+    }
 
     public Item(int id, @NonNull String name) {
         this.id = id;
         this.name = name;
     }
 
-    @ToString.Include
-    String created() {
-        return FORMATTER.format(created);
+    public Item(int id, String name, LocalDateTime created) {
+        this.id = id;
+        this.name = name;
+        this.created = created;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("id: %s, name: %s, created: %s", id, name, FORMATTER.format(created));
     }
 
 }
